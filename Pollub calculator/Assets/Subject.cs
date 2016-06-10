5012bdf25b57f6ab084e958f0e97c17e9130c47a
Rafał Maksim <rafalx117@gmail.com>
2016-06-10 20:46:43 +0200

    first commit

---
+++ Pollub calculator/Assets/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pollub_calculator.Assets
+{
+    class Subject
+    {
+        public float Grade;
+        public string Name;
+
+       public Subject()
+        {
+            Grade = -1;
+            Name = "brak";
+        }
+    }
+
+}
--- Pollub calculator/MainPage.xaml.cs
+++ Pollub calculator/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Pollub_calculator.Assets;
+
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +24,36 @@ namespace Pollub_calculator
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
     public sealed partial class MainPage : Page
     {
+        List<Subject> subjects = new List<Subject>();
+
         public MainPage()
         {
             this.InitializeComponent();
+            CreateControls();
+            
+
+        }
+
+        private void CreateControls()
+        {
+            TextBlock txtName = new TextBlock();
+            txtName.Text = "@isenthil";
+            Button btnClick = new Button();
+            btnClick.Content = "Click";
+            btnClick.Click += new RoutedEventHandler(btnClick_Click);
+           
+            panel.Children.Add(txtName);
+            panel.Children.Add(btnClick);
+        }
+
+        void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("ello");
         }
     }
 }
+
